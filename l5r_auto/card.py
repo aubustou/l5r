5127d3d0c8316f596ba8905d6142d888001654b2
aubustou <survivalfr@yahoo.fr>
2023-08-28 01:11:30 +0200

    Additional

--- l5r_auto/card.py
+++ l5r_auto/card.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from typing import List, Type
+from dataclasses import dataclass, field, fields, Field
+from typing import Any, Type
+from l5r_auto.locations import DynastyDiscard, RemovedFromGame
+
+from l5r_auto.player import Entity
 
 
 @dataclass(kw_only=True)
@@ -24,6 +27,37 @@ class Trait(Action):
 @dataclass(kw_only=True)
 class Card:
     id: int
+
+    entity_type: Type[Entity] = field(init=False)
+
+    def written(self) -> dict[str, Any]:
+        return {
+            f.name: getattr(self, f.name)
+            for f in fields(self)
+            if f.metadata.get("is_written")
+        }
+
+    def __call__(self, *args: Any, **kwds: Any) -> Entity:
+        return self.entity_type(*args, **kwds, **self.written())
+
+    def target(self):
+        pass
+
+
+@dataclass(kw_only=True)
+class DynastyCard(Card):
+    bowed: bool = False
+
+    def destroy(self):
+        self.location = DynastyDiscard
+
+    def remove_from_game(self):
+        self.location = RemovedFromGame
+
+
+@dataclass(kw_only=True)
+class FateCard(Card):
+    pass
 
 
 @dataclass(kw_only=True)
--- l5r_auto/cards/personalities/common.py
+++ l5r_auto/cards/personalities/common.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from typing import Type
-from l5r_auto.card import Card, Keyword
+from l5r_auto.card import Card, DynastyCard, Keyword
 from l5r_auto.card import Ability, Trait
 from copy import copy
+from l5r_auto.locations import Deck, Location
 from l5r_auto.player import Entity
+from l5r_auto.clans import Clan
 
 
 def SoulOf(title: str) -> Type[Keyword]:
@@ -17,20 +19,37 @@ def SoulOf(title: str) -> Type[Keyword]:
     return klass
 
 
-@dataclass(kw_only=True)
-class Personality(Card):
-    title: str
-    force: int
-    chi: int
-    honor_requirement: int | None
-    personal_honor: int
-    gold_cost: int
-    clan: str
-    keywords: list[Type[Keyword]] = field(default_factory=list)
-    traits: list[Trait] = field(default_factory=list)
-    abilities: list[Ability] = field(default_factory=list)
+class HasZeroChi(Ability):
+    def has_zero_chi(self, personality: PersonalityEntity):
+        if personality.chi == 0:
+            personality.destroy()
 
 
 @dataclass(kw_only=True)
-class PersonalityEntity(Entity):
-    location: str = "deck"
+class Personality(DynastyCard):
+    title: str = field(metadata={"is_written": True})
+    force: int = field(metadata={"is_written": True})
+    chi: int = field(metadata={"is_written": True})
+    honor_requirement: int | None = field(metadata={"is_written": True})
+    personal_honor: int = field(metadata={"is_written": True})
+    gold_cost: int = field(metadata={"is_written": True})
+    clan: list[Type[Clan]] = field(metadata={"is_written": True})
+
+    keywords: list[Type[Keyword]] = field(
+        default_factory=list, metadata={"is_written": True}
+    )
+    traits: list[Trait] = field(default_factory=list, metadata={"is_written": True})
+    abilities: list[Ability] = field(
+        default_factory=list, metadata={"is_written": True}
+    )
+
+    def __post_init__(self):
+        self.entity_type = PersonalityEntity
+
+
+@dataclass(kw_only=True)
+class PersonalityEntity(Entity, Personality):
+    location: Type[Location] = Deck
+
+    def bow(self):
+        self.bowed = True
